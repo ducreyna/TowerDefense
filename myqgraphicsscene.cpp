7ee24857ac8328013abda858d86279a959d1ea3a
ducreyna <ducrey.nathan@gmail.com>
2011-05-17 17:00:43 +0200

    emission du signal MouseTracking dans MouseMoveEvent

--- myqgraphicsscene.cpp
+++ myqgraphicsscene.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-MyQGraphicsScene::MyQGraphicsScene(QGraphicsView *parent):QGraphicsScene(parent),tourDemandee("")
+MyQGraphicsScene::MyQGraphicsScene(QGraphicsView *parent):QGraphicsScene(parent),tourDemandee(""),precedent(0)
 {}
 
 void MyQGraphicsScene::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
@@ -31,7 +31,7 @@ void MyQGraphicsScene::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
             emit ajouterTour(x,y,"PETANQUE");
         }
     }
-    else if(I->data(0).toString() == "EAU")
+    else if(I->data(0).toString() == "EAU" || I->data(0).toString() == "PIERRE" || I->data(0).toString() == "PEINTURE" || I->data(0).toString() == "PETANQUE")
     {
         removeItem(I);
     }
@@ -39,10 +39,41 @@ void MyQGraphicsScene::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
 
 void MyQGraphicsScene::mouseMoveEvent(QGraphicsSceneMouseEvent *mouseEvent)
 {
-    /*int x = mouseEvent->scenePos().x();
+    int x = mouseEvent->scenePos().x();
     int y = mouseEvent->scenePos().y();
 
-    std::cout << x << " " << y << std::endl;*/
+    QGraphicsItem * I = itemAt(x,y);
+
+    std::cout << qPrintable(I->data(0).toString()) << std::endl;
+
+    if(I->data(0).toString() == "HERBE")
+    {
+        if(tourDemandee == "EAU")
+        {
+            emit tourMouseTracking(x,y,"EAU");
+            precedent = itemAt(x,y);
+        }
+        else if(tourDemandee == "PIERRE")
+        {
+            emit tourMouseTracking(x,y,"PIERRE");
+
+        }
+        else if(tourDemandee == "PEINTURE")
+        {
+            emit tourMouseTracking(x,y,"PEINTURE");
+        }
+        else if(tourDemandee == "PETANQUE")
+        {
+            emit tourMouseTracking(x,y,"PETANQUE");
+        }
+    }
+    else if(I->data(0).toString() == "EAU" || I->data(0).toString() == "PIERRE" || I->data(0).toString() == "PEINTURE" || I->data(0).toString() == "PETANQUE")
+    {
+        //removeItem(I);
+    }
+    if(precedent != 0) removeItem(precedent);
+
+    //std::cout << x << " " << y << std::endl;
 }
 
 void MyQGraphicsScene::setTourDemandee(std::string tour)
